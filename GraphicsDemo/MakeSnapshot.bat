7c662319e81e6b3580a1aabeab61b03dab44ff36
kg1992 <kyunggeun1992@gmail.com>
2019-03-07 15:39:23 +0900

    Project will make snapshot directory and fill the content on Post-Build event.
    PostBuild.bat - line added to call MakeSnapshot.bat
    MakeSanpshot.bat - generates snapshot directory. Version name has to be chagned manually.

---
+++ GraphicsDemo/MakeSnapshot.bat
@@ -0,0 +1,18 @@
+set PLATFORM_TARGET=%1
+set CONFIGURATION=Release
+set VERSION=0.0.1
+rem https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/call#batch-parameters
+set SNAPSHOT_LOCATION="..\Snapshot\%VERSION%\"
+set SHADERS=*.glsl
+set RESOURCES=.\Resources
+set FONTS=.\Fonts
+set BUILD_LOCATION="..\%PLATFORM_TARGET%\%CONFIGURATION%\"
+ 
+echo %SHADERS%
+rem make directory for snapshot
+mkdir %SNAPSHOT_LOCATION%
+rem copy shaders
+xcopy /y %SHADERS% %SNAPSHOT_LOCATION%
+xcopy /y /s %RESOURCES% %SNAPSHOT_LOCATION%\Resources\
+xcopy /y %BUILD_LOCATION%* %SNAPSHOT_LOCATION%
+xcopy /y /s %FONTS% %SNAPSHOT_LOCATION%\Fonts\
