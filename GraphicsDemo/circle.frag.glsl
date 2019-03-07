7c524399fd741b3719a8880c3f90c861836ff47e
kg1992 <kyunggeun1992@gmail.com>
2019-03-07 13:22:43 +0900

    circle.frag.glsl - used for point light visualization. Now shows sun icon instead of a circle.

--- GraphicsDemo/circle.frag.glsl
+++ GraphicsDemo/circle.frag.glsl
@@ -3,19 +3,54 @@
 in vec2 vUv;
 in vec3 vPosition;
 
-out vec4 color;
+out vec4 FragColor;
+
+float Circle(vec2 uv, float r, float blur)
+{
+    float d = length(uv);
+    
+    float c = smoothstep(r, r - blur, d);
+    
+    return c;
+}
+
+float Sun(vec2 uv, vec2 offset, float scale)
+{
+    const float radius = 0.4;
+    const float petalDistance = 0.6;
+    const float petalRadius = 0.12;
+    const float blur = 0.01;
+    const int petalCount = 10;
+    
+    float c = Circle(uv, radius * scale, blur);
+    
+    
+    for( int i = 0; i < petalCount; ++i )
+    {
+        float angle = radians(float(360/petalCount*i));
+        vec2 center = vec2(cos(angle), sin(angle)) * petalDistance;
+        c += Circle(uv - center * scale, petalRadius * scale, blur);
+    }
+    
+    return c;
+}
+
+vec3 sun(vec2 uv)
+{
+    uv  -= 0.5;  // -0.5 <> 0.5
+    
+    const float scale = 0.71;
+    float c = Sun(uv, vec2(0.0), scale);
+     
+    return c;
+}
 
 void main(void)
 {
-	vec2 auv = vUv * 2 - 1;
+    vec3 c = sun(vUv);
 
-	float distance = length(auv);
+    if( c.r < 0.1 )
+        discard;
 
-	float begin = 0.4;
-	float end = 0.3;
-	float t = smoothstep(begin, end, distance);
-	if( distance > end )
-		discard;
-
-	color = vec4( t, t, t, 1.0 );
+    FragColor = vec4(c, 1.0);
 }
